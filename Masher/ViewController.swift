a46b3d35edf1cd39de9fcad9c67169a4cfa51946
Thomas Your <thomas.your@gmail.com>
2019-04-06 16:08:48 -0700

    Setting up the project.

---
+++ Masher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Masher
+//
+//  Created by Thomas Your on 4/6/19.
+//  Copyright © 2019 Thomas Your. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
